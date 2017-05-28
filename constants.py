4d0905ae8358dccb7765abe4e5266ac55b7aeae0
John <me@joconnor.xyz>
2017-05-28 20:52:30 +0000

    reset pins

--- constants.py
+++ constants.py
@@ -3,9 +3,9 @@
 # relative to pwd or absolute
 LOG_FILE_PATH = "/home/pi/pi-feeder/pifeeder.log"
 
-GPIO_PIN_ENABLE = 17
-GPIO_PIN_FORWARD = 18
-GPIO_PIN_BACKWARD = 23
+GPIO_PIN_ENABLE = 18
+GPIO_PIN_FORWARD = 4
+GPIO_PIN_BACKWARD = 17
 
 MOTOR_DEFAULT_DURATION = 1.1
 
